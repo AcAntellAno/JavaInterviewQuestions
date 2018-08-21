887c1c34ac6bbb91f72ccd74517cfb0d5ae96b3f
AcAntellAno <acantellano1996@gmail.com>
2018-08-20 20:06:36 -0700

    Style changes

--- GroupSite/GroupSite.js
+++ GroupSite/GroupSite.js
@@ -1,24 +1,21 @@
+function display1() {
 
-
-function display1(){
-	
-	var reveal= document.getElementById("answer1");
-	if(reveal.style.display == "none")
-    reveal.style.display = "block";
+	var reveal = document.getElementById("answer1");
+	if (reveal.style.display == "none")
+		reveal.style.display = "block";
 	else
-    reveal.style.display = "none";
+		reveal.style.display = "none";
 
 
 }
 
-function display2(){
+function display2() {
 
-	var reveal2=document.getElementById("answer2");
-	if(reveal2.style.display==="none"){
-	reveal2.style.display = "block";
-	}
-	else{
-	reveal2.style.display = "none";
+	var reveal2 = document.getElementById("answer2");
+	if (reveal2.style.display === "none") {
+		reveal2.style.display = "block";
+	} else {
+		reveal2.style.display = "none";
 	}
 
 }
